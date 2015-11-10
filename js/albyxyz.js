52760429072c5f9cdbf12470958de08e1fc879bd
Alberto Nicoletti <albyx.n@gmail.com>
2015-11-10 22:27:27 +0100

    Cross browser compatibility

--- js/albyxyz.js
+++ js/albyxyz.js
@@ -1,9 +1,25 @@
 var app = angular.module( 'Albyxyz', [ 'ngMaterial' ] );
 
-app.controller('HomeController', function($scope) {
+app.controller('HomeController', function($scope, $mdToast) {
   $scope.expandPage = false;
+  $scope.matteDay = "";
   setTimeout(function(){
   	$scope.expandPage = true;
   	$scope.$apply();
-  }, 3500);
+  	init();
+  }, 2500);
+
+  function init() {
+  	if(new Date().getDay() == 4) {
+	  	$scope.matteDay = 'Today is the "sfancula matte day"! Celebrate with us and post a "vaffanculo matte" on his Facebook page!';
+	  	var toast = $mdToast.simple()
+	          .content('Celebrate with us!')
+	          .action('OK')
+	          .highlightAction(true)
+	          .position("bottom right");
+	    $mdToast.show(toast).then(function() {
+
+	    });
+	  }
+  }
 });
